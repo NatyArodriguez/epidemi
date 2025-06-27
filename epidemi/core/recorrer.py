1f4c6f500773e92d581404e4a92bfe02d99a4af8
Natalia Rodriguez <natalia.agustina.rodriguez@mi.unc.edu.ar>
2025-06-27 18:46:30 -0300

    division de recorrer en el caso de maximo por columna y el maximo de la media por filas

--- epidemi/core/recorrer.py
+++ epidemi/core/recorrer.py
@@ -1,4 +1,82 @@
+""" travers is a function to agroup the columns of a matrix by ten"""
+
 import numpy as np
+
+
+def travers_column(array):
+    """With the max by columns, obtain mean and standar desviation.
+
+    Args:
+        array (ndarray) : Array with your data
+
+    Returns:
+        [shapes, mean, std] (list)
+    """
+    
+    if not isinstance(array, np.ndarray):
+        raise TypeError("Input must be a numpy ndarray.")
+    
+    if array.shape[1] % 10 != 0:
+        raise ValueError("Numer of columns must be a multiple of 10.")
+    
+    rango = array.shape[1]
+    step = rango//10
+    
+    max_values_column = np.empty(step)
+    std_values_column = np.empty(step)
+    shapes = np.arange(10, rango+10, 10)
+    
+    for i in range(step):
+        if i < rango:
+            matriz= array[:,0:i+10]
+            max_columns = np.max(matriz,axis=0)
+            x = np.mean(max_columns)
+            y = np.std(max_columns)/np.sqrt(shapes[i])
+            
+            max_values_column[i] = x
+            std_values_column[i] = y
+            
+    return [shapes, max_values_column, std_values_column]
+
+
+def travers_mean(array):
+    """With the max of the average of the columns, obtain mean and standar\
+        desviation.
+
+    Args:
+        array (ndarray) : Array with your data
+
+    Returns:
+        [shapes, mean, std] (list)
+    """
+    
+    if not isinstance(array, np.ndarray):
+        raise TypeError("Input must be a numpy ndarray.")
+    
+    if array.shape[1] % 10 != 0:
+        raise ValueError("Numer of columns must be a multiple of 10.")
+    
+    rango = array.shape[1]
+    step = rango//10
+    
+    max_values_mean = np.empty(step)
+    std_values_mean = np.empty(step)
+    shapes = np.arange(10, rango+10, 10)
+    
+    for i in range(step):
+        if i < rango:
+            matriz= array[:,0:i+10]
+
+            u = np.mean(matriz, axis=1)
+            v = np.std(matriz, axis =1)
+            
+            p_max = u.max()
+            where_max = np.where(u == p_max)
+            std_max = v[where_max]/np.sqrt(shapes[i])
+
+            max_values_mean[i] = p_max
+            std_values_mean[i] = std_max
+    return [shapes, max_values_mean, std_values_mean]
 
 
 def recorrer(array):
